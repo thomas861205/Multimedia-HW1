e0ca0b282589b4b66b752c7160b17d83908af20d
Thomas <thomaschou861205@gmail.com>
2019-04-05 12:14:29 +0800

    1D DFT succeed

--- DCT_img_com.m
+++ DCT_img_com.m
@@ -8,6 +8,32 @@ YIQ_img = RGB2YIQ(RGB_img);
 % YIQ_testbench = rgb2ntsc(RGB_img);
 
 
+% input = [0, 0, 0, 153, 255, 255, 220, 220]';
+% output = OneD_DCT(input)
+
+function ret = C(u)
+	if u == 0
+		ret = sqrt(2)/2;
+	else
+		ret = 1;
+	end
+end
+
+function frequency = OneD_DCT(spatial)
+	[height, width] = size(spatial);
+    frequency = zeros(height, width);
+    for u = 0:height-1
+    	tmp = 0;
+    	for r = 0:height-1
+    		tmp = tmp + sqrt(2/height)*C(u) * spatial(r+1) * cos((2*r+1)*u*pi/(2*height));
+    	end
+    	frequency(u+1) = tmp;
+    end
+end
+
+function frequency = TwoD_DCT(spatial)
+end
+
 function YIQ_img = RGB2YIQ(RGB_img)
 	[height, width, ~] = size(RGB_img);
 	YIQ_img = zeros(height, width, 3);
