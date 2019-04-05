a5ff0111d56023956efd3ffdd2bb27c3ae2d3bb0
Thomas <thomaschou861205@gmail.com>
2019-04-05 11:37:52 +0800

    add RGB2YIQ function

--- DCT_img_com.m
+++ DCT_img_com.m
@@ -1,5 +1,23 @@
 clear all;
 clc;
 
-img = imread('cat1.png');
-image(img)
+RGB_img = double(imread('cat1.png'));
+% image(RGB_img)
+% [height, width, ~] = size(RGB_img)
+YIQ_img = RGB2YIQ(RGB_img);
+% YIQ_testbench = rgb2ntsc(RGB_img);
+
+
+function YIQ_img = RGB2YIQ(RGB_img)
+	[height, width, ~] = size(RGB_img);
+	YIQ_img = zeros(height, width, 3);
+	RGB2YIQmat = [
+		0.299, 0.587,  0.114;
+		0.596, -0.275, -0.321;
+		0.212, -0.523, 0.311];
+	for h = 1:height
+		for w = 1:width
+			YIQ_img(h, w, :) = RGB2YIQmat * reshape(RGB_img(h, w, :),[],1);
+		end
+	end
+end
