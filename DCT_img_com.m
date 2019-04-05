26d5a41d34b5098d2d0ca0931d3fc98814ae5b97
Thomas <thomaschou861205@gmail.com>
2019-04-05 13:47:51 +0800

    2D DFT incomplete

--- DCT_img_com.m
+++ DCT_img_com.m
@@ -11,6 +11,16 @@ YIQ_img = RGB2YIQ(RGB_img);
 % input = [0, 0, 0, 153, 255, 255, 220, 220]';
 % output = OneD_DCT(input)
 
+input = [255, 255, 255, 255, 255, 255, 159, 159;
+		 255,   0,   0,   0, 255, 255, 159, 159;
+		 255,   0,   0,   0, 255, 255, 255, 255;
+		 255,   0,   0,   0, 255, 255, 255, 255;
+		 255, 255, 255, 255, 255, 255, 100, 255;
+		 255, 255, 255, 255, 255, 255, 100, 255;
+		 255, 255, 255, 255, 255, 255, 100, 255;
+		 255, 255, 255, 255, 255, 255, 100, 255];
+output = TwoD_DCT(input)
+
 function ret = C(u)
 	if u == 0
 		ret = sqrt(2)/2;
@@ -32,6 +42,22 @@ function frequency = OneD_DCT(spatial)
 end
 
 function frequency = TwoD_DCT(spatial)
+	[height, width, layer] = size(spatial)
+	frequency = zeros(height, width, layer);
+	for l = 1:layer
+		for u = 0:height-1
+			for v = 0:width-1
+				tmp = 0;
+				for r = height-1
+					for s = width-1
+						tmp = tmp + 2/sqrt(height*width)*C(u)*C(v) * spatial(r,s,l) *...
+						 cos((2*r+1)*u*pi/(2*height))*cos((2*s+1)*v*pi/(2*width));
+					end
+				end
+				frequency(u+1, v+1, l) = tmp;
+			end
+		end
+	end
 end
 
 function YIQ_img = RGB2YIQ(RGB_img)
